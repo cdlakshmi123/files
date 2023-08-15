e45f276dc6abce17db81f1dfbd1fe2525f17f853
Your Name <you@example.com>
2023-08-15 15:00:12 +0530

    files

---
+++ l.py
@@ -0,0 +1,15 @@
+# age=input("")
+# f=open("data.txt","w")
+# f.write(age)
+# f.close()
+
+# f=open("data.txt","r")
+# print(f.read())
+# f.close()
+
+
+# date=int(input(""))
+# f=open("data.txt","w")
+# f.write(date)
+# f.close()
+
